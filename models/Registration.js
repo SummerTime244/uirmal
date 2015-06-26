9613b2d8b720b959bb9afe6738c98a34df7dbc11
Shahein <shaheinm@gmail.com>
2015-06-26 01:50:32 -0400

    Fixing startup issues

--- models/Registration.js
+++ models/Registration.js
@@ -14,14 +14,14 @@ var Registration = new keystone.List('Registration', {
 Registration.add({
 	name: { type: Types.Name, required: true },
 	school: {type: String, initial: true, required: true},
-	email: { type: Types.Email, required: true },
+	email: { type: Types.Email, initial: true, required: true },
 	phone: { type: String },
-	numOfStudents: { type: String, required: true },
-	country1: { type: Types.Select, options: 'Algeria, Bahrain, Comoros, Djibouti, Egypt, Iraq, Jordan, Kuwait, Lebanon, Libya, Mauritania, Morocco, Oman, Palestine, Qatar, Saudi Arabia, Somalia, Sudan, Syria, Tunisia, UAE, Yemen', required: true },
-	country2: { type: Types.Select, options: 'Algeria, Bahrain, Comoros, Djibouti, Egypt, Iraq, Jordan, Kuwait, Lebanon, Libya, Mauritania, Morocco, Oman, Palestine, Qatar, Saudi Arabia, Somalia, Sudan, Syria, Tunisia, UAE, Yemen', required: true },
-	country3: { type: Types.Select, options: 'Algeria, Bahrain, Comoros, Djibouti, Egypt, Iraq, Jordan, Kuwait, Lebanon, Libya, Mauritania, Morocco, Oman, Palestine, Qatar, Saudi Arabia, Somalia, Sudan, Syria, Tunisia, UAE, Yemen', required: true },
+	numOfStudents: { type: String, initial: true, required: true },
+	country1: { type: Types.Select, options: 'Algeria, Bahrain, Comoros, Djibouti, Egypt, Iraq, Jordan, Kuwait, Lebanon, Libya, Mauritania, Morocco, Oman, Palestine, Qatar, Saudi Arabia, Somalia, Sudan, Syria, Tunisia, UAE, Yemen', initial: true, required: true },
+	country2: { type: Types.Select, options: 'Algeria, Bahrain, Comoros, Djibouti, Egypt, Iraq, Jordan, Kuwait, Lebanon, Libya, Mauritania, Morocco, Oman, Palestine, Qatar, Saudi Arabia, Somalia, Sudan, Syria, Tunisia, UAE, Yemen', initial: true, required: true },
+	country3: { type: Types.Select, options: 'Algeria, Bahrain, Comoros, Djibouti, Egypt, Iraq, Jordan, Kuwait, Lebanon, Libya, Mauritania, Morocco, Oman, Palestine, Qatar, Saudi Arabia, Somalia, Sudan, Syria, Tunisia, UAE, Yemen', initial: true, required: true },
 	tourPackage: { type: Types.Boolean, initial: true, index: true },
-	message: { type: Types.Markdown, required: true },
+	message: { type: Types.Markdown },
 	createdAt: { type: Date, default: Date.now }
 });
 
